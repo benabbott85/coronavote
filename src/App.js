376711df866e2f63cbef858e90ee418b0dc714d9
Ben Abbott <babbott85@gmail.com>
2020-04-02 13:56:10 -0600

    navbar now appearing on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import NavTabs from "./components/NavTabs";
-import logo from './logo.svg';
+import Home from "./components/pages/Home";
+import Signout from "./components/pages/Signout";
+import Signup from "./components/pages/Signup";
+import Verify from "./components/pages/Verify"
+// import logo from './logo.svg';
 // import './App.css';
 
 function App() {
@@ -9,7 +13,7 @@ function App() {
    <Router>
      <div>
        <NavTabs/>
-       <Route exact path ="/" component={Home}/>
+       <Route exact path ="/home" component={Home}/>
        <Route exact path ="/signup" component ={Signup}/>
        <Route exact path = "/verify" component = {Verify}/>
        <Route exact path = "/signout" component ={Signout}/>
