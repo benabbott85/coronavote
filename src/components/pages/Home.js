1b63594b900f791c46f4162257c16fe894579872
Ben Abbott <babbott85@gmail.com>
2020-04-04 18:16:24 -0600

    troubleshooting home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 function Home (){
     return (
         <div>
-            
+            <h1 className="welcome">Welcome to CoronaVote! The only way to vote during this pandemic</h1>
+            <br></br>
+            <p className="if"> Please sign in if you have already created an account otherwise click on the signup button and follow the prompts</p>
         </div>
     )
 }
