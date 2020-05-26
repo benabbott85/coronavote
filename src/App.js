a971e64f5d2f629184a23067234335786713ad6c
Ben Abbott <babbott85@gmail.com>
2020-05-26 10:45:57 -0600

    signin inputs added on home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import { Container, Row, Col } from "reactstrap"
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import Signout from "./components/pages/Signout";
 import Signup from "./components/pages/Signup";
-import Verify from "./components/pages/Verify"
+import Verify from "./components/pages/Verify";
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -13,10 +14,10 @@ function App() {
    <Router>
      <div>
        <NavTabs/>
-       <Route exact path ="/home" component={Home}/>
-       <Route exact path ="/signup" component ={Signup}/>
-       <Route exact path = "/verify" component = {Verify}/>
-       <Route exact path = "/signout" component ={Signout}/>
+       <Route exact path ="/Home" component={Home}/>
+       <Route exact path ="/Signup" component ={Signup}/>
+       <Route exact path = "/Verify" component = {Verify}/>
+       <Route exact path = "/Signout" component ={Signout}/>
      </div>
    </Router>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,76 @@
 import React from "react";
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from "reactstrap";
+
 
 function Home (){
     return (
-        <div>
-            <h1 className="welcome">Welcome to CoronaVote! The only way to vote during this pandemic</h1>
-            <br></br>
-            <p className="if"> Please sign in if you have already created an account otherwise click on the signup button and follow the prompts</p>
-        </div>
+        // <div>
+        //     <h1 className="welcome">Welcome to CoronaVote! The only way to vote during this pandemic</h1>
+        //     <br></br>
+        //     <p className="if"> Please sign in if you have already created an account otherwise click on the signup button and follow the prompts</p>
+        // </div>
+        <Container>
+        <Row>
+        <h1 className="weclome"> Welcome to CoronaVote! The only way to vote during this pandemic</h1>
+        </Row>
+        <Row>
+            <p className="if">Please sign in if you have already created an account otherwise click on the signup button and create an account</p>
+        </Row>
+        <h2>Sign In:</h2>
+        <Row>
+           
+            <Form>
+                <FormGroup>
+                    <Label for = "signIn" hidden> Sign In:</Label>
+                    <Input
+                     type="text"
+                     name="username"
+                     id="userrname"
+                     placeholder="username" 
+                    >
+                    {/* <Input
+                    type="text"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+
+
+                    >
+
+                    </Input> */}
+                   
+                    </Input>
+                </FormGroup>
+            </Form>
+        </Row>
+        <Row>
+        <Form>
+                <FormGroup>
+                    <Label for = "signIn" hidden> Sign In:</Label>
+                    <Input
+                     type="text"
+                     name="password"
+                     id="password"
+                     placeholder="password" 
+                    >
+                    {/* <Input
+                    type="text"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+
+
+                    >
+
+                    </Input> */}
+                   
+                    </Input>
+                </FormGroup>
+                <Button id="signin">Sign In</Button>
+                <Button id="signup">Sign Up</Button>
+            </Form>
+        </Row>
+        </Container>
     )
 }
 
