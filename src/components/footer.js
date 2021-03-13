a2a8634bba24c1c0e97813434d11a1257e595e65
Ben Abbott <babbott85@gmail.com>
2021-03-13 11:15:36 -0700

    updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function footer (){
     return (
         <h4 className="footer">Copywright @ VoteFromHome.com 
         <br/>
-        <p>test</p>
+     
         Developed by: Ben Abbott
         </h4>
     )
